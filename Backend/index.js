d5d8b71f6da802d6d64ce0dd4b166649f2f69baf
Sahil <sahilfrost@gmail.com>
2023-07-26 15:09:47 +0530

    Added models and Routes

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("inotebook");
-})
+
+//Available Routes
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
 
 app.listen(port,()=>{
     console.log(`inotebook listening at http://localhost:${port}`)
