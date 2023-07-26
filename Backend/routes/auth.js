8ca48edeb12cbb0b48da60ddd830c26f1562ce56
Sahil <sahilfrost@gmail.com>
2023-07-26 19:18:28 +0530

    Password encryption and jsonwebtoken added

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+ const JWT_SECRET = 'Sahilgithublunatic@1'
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post(
@@ -27,14 +31,22 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists!" });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
 
       //Else create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        user:{
+            id: user.id
+        }
+      }
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({authToken});
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error occured!");
