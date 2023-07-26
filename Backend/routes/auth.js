eb84c7f43b2cebf6b123e05b9972fcb41c22ad0c
Sahil <sahilfrost@gmail.com>
2023-07-26 19:32:36 +0530

    Created login endpoint

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -49,9 +49,50 @@ router.post(
       res.json({authToken});
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured!");
+      res.status(500).send("Internal server Error!");
     }
   }
 );
 
+// Authenticate a User using: POST "/api/auth/login". No login required
+router.post(
+    "/login",
+    [
+      body("email", "Enter a valid email").isEmail(),
+      body("password", "Password cannot be blank").exists()
+    ],
+    async (req, res) => {
+        // If there are errors, return Bad request alongside errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email, password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error: "Wrong Credentials"});
+
+        }
+
+        const passwordCompare = await bcrypt.compare(password,user.password);
+        if(!passwordCompare){
+            return res.status(400).json({error: "Wrong Credentials"});
+        }
+
+        const data = {
+            user:{
+                id: user.id
+            }
+          }
+          const authToken = jwt.sign(data, JWT_SECRET);
+          res.json({authToken});
+
+    } catch (error) {
+        console.error(error.message);
+      res.status(500).send("Internal server Error!");
+    }
+
+})
 module.exports = router;
